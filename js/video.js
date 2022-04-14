4ec19dafefb24eefdd79f7ace46a5d51d7844800
Chloe Zhang <xiaozh@Chloes-MacBook-Pro.local>
2022-04-14 17:07:29 -0400

    finish

---
+++ js/video.js
@@ -0,0 +1,71 @@
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+
+var video = document.querySelector("#player1")
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video.autoplay = false
+	video.loop = false
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play()
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%"
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause()
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *= 0.95
+	console.log("Now the speed is " + video.playbackRate) 
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate /= 0.95
+	console.log("Now the speed is " + video.playbackRate) 
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Original location " + video.currentTime) 
+	if (video.currentTime + 15 > video.duration){
+		console.log("Go back to the beginning")
+		video.currentTime = 0 
+		console.log("New location 0") 
+	}
+	else{
+		video.currentTime += 15
+		console.log("New location " + video.currentTime) 
+	}
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("The value of mute is " + video.muted) 
+	if (video.muted){
+		video.muted = false
+		document.querySelector("#mute").innerHTML = "Mute"
+	}
+	else{
+		video.muted = true
+		document.querySelector("#mute").innerHTML = "Unmute"
+	}
+});
+
+document.querySelector("#slider").addEventListener("click", function() {
+	console.log(this.value) 
+	video.volume = this.value * 0.01
+	document.querySelector("#volume").innerHTML = this.value + "%"
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	document.getElementById("player1").classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	document.getElementById("player1").classList.remove("oldSchool");
+});
